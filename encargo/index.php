3f1672ac044de3f16649f676123f6bcadc55c3af
Enrique Mendoza <systemsadms@hotmail.com>
2020-01-27 03:29:17 -0400

    Diseno y Mquetado

---
+++ encargo/index.php
@@ -0,0 +1,607 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Encargo Envíos y Fletes</title>
+  <meta charset="UTF-8">
+  <meta name=”description” content="Encargo envios y fletes es una empresa de envios areos y martimos ubicada en pana con especialidad en envios a Venezuela" />
+  <meta name="keywords" content="envios, areo, maritmo, panama, venezuela"/>
+  <meta name="author" content="Systems Admins C.A" />
+  <meta name="copyright" content="Encargo Envios y FLetes" />
+  <meta name="robots" content="index, follow">
+  <meta name="google" content="nositelinkssearchbox">
+
+
+
+
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="js/paralax.js"></script>
+
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	 <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><img src="img/logo.png" alt="logo" width="100px"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#servicios">Servicios</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#comoEnviar">¿Como Enviar?</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contacto">Contacto</a>
+                </li>
+                <!--<li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>-->
+              </ul>
+            </div>
+          </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="verMasPrimary" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Ver Más</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="imgSize"></div>
+      
+        <p>
+          ENCARGO es una empresa de transporte y flete internacional líder en envíos
+ 
+          de encomiendas y compras on-line ofreciendo el traslado a través 
+          de envíos
+           aéreos y marítimo.
+
+          Nuestro objetivo es enfocarnos y trabajar de forma segura,
+           para 
+          que nuestros clientes sientan la tranquilidad de que sus envíos lleguen
+           a
+           su destino en corto tiempo y en perfecto estado. 
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="aereo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Aéreo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <div class="imgSizeAereo">          
+         </div>
+         Salidas aéreas cada 10 dias , y tiempo de entregas son 7 dias habliles y el costo sera cotizado por el peso de la encomienda 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="maritimo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Marítimo</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="imgSizeMaritimo"></div>
+        <br>
+        <p>
+          Ofrecemos un servicio de envíos cada 15 días vía marítima a venezuela
+
+           Este servicio le permitirá a usted enviar y recibir paquetes a gran escala
+           sin 
+          importar el peso, manejamos excelentes tarifas, y contará con la tranquilidad 
+
+          de saber que su encomienda está a salvo en nuestras manos.
+
+          Una vez zarpe el barco
+           desde el puerto con la fecha programada se contaran
+           de 20 a 25 días continuos
+           para la entrega de su encomienda. 
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="puerta" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Puerta</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="imgSizePuerta"></div>
+        <br>
+        <p>
+        Tu comodidad siempre es lo primero, por lo cual, 
+        contamos con un servicio
+         de entrega de tu encomienda en tu
+         hogar u oficina.
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container-fluid">
+
+
+
+	<div class="row">
+      <div class="col-lg-12">
+       <div class="caja-banner">
+
+
+           <div class="caja-banner-contenido">
+           	
+              <h1>ENCARGO ENVÍOS Y FLETES</h1> 
+              <h3>SERVICIOS PUERTA A PUERTA</h3>
+
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#verMasPrimary" style="background-color: #f95908; border:none;">
+                    VER MÁS
+                  </button>
+             
+           </div>
+		
+
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+ <div class="row" id="nosotros">
+      <div class="col-lg-12 enviosVenezuela">
+        <div class="componente-envios">
+            <h3>ENVÍOS A VENEZUELA DESDE PANAMÁ</h3>
+            <p>Somos mas que una empresa, somos tu aliado. Actualmente nos encontramos en PANAMÁ,
+              <BR>
+            brindando nuestro servicio puerta a puerta de encomiendas y paquetes a VENEZUELA</p>
+
+            <H5 style="margin-top: 40px;">REALIZA TUS ENVÍOS A VENZUELA SIN INCONVENIENTES ¡ES MUY SENCILLLO!</H5>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <div class="row">
+      <div class="col-md-8"  style="background-color:white; height: 300px;">
+        <div class="componente-loLLevamos">
+            <h3>TE LO LLEVAMOS A DONDE ESTES</h3>
+            <p style="text-align: justify;">Nos tomamos muy enserio la comodidad de nuestros clientes, por lo cual nos ofrecemos a llevarte todos los pedidos que hagas hasta la puerta de tu casa</p>
+        </div>
+      </div>
+      <div class="col-md-4 componente-envios-img"  style="background-color:red; height: 300px;"> 
+      </div>
+    </div>
+
+
+    <div class="row econmiendasDesktop">
+      <div class="col-md-4 componente-envios-img2"  style="background-color:red; height: 300px;">
+      </div>
+      <div class="col-md-8 tusEncomiendas">
+        <div class="componente-loLLevamos">
+            <h3>TUS ENCOMIENDAS ESTAN SEGURAS</h3>
+            <p style="text-align: justify;">Para nosotros la seguridad es lo primero, por lo tanto te damos plena certeza de que tus compras y envíos estaran muy protegidos</p>
+        </div>
+      </div>
+    </div>
+
+
+
+<div class="econmiendasMovil">
+     <div class="row">     
+      <div class="col-md-8 tusEncomiendas">
+        <div class="componente-loLLevamos">
+            <h3>TUS ENCOMIENDAS ESTAN SEGURAS</h3>
+            <p style="text-align: justify;">Para nosotros la seguridad es lo primero, por lo tanto te damos plena certeza de que tus compras y envíos estaran muy protegidos</p>
+        </div>
+      </div>
+       <div class="col-md-4 componente-envios-img2"  style="background-color:red; height: 300px;">
+      </div>
+    </div>
+</div>
+
+
+
+
+
+    <div class="row" id="servicios">
+      <div class="col-lg-12"  style="background-color:white; height: 150px;">
+        <div class="componente-envios">
+            <h3>SERVICIOS</h3>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="row">
+    <div class="col-lg-4"  style="background-color:white; height: 450px;">        
+            <div class="serv-air">
+                <img src="img/aereo.png" alt="Servicio de Envio Aereo"/>
+                <h3>AÉREO</h3>
+                <p>Contamos con servicios rapido de envíos aéreos para que recibas todos tus paquetes lo más rapido posible</p>
+                <!--Bootstrap Buton-->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#aereo" style="background-color: #f95908; border:none;">
+                  VER MÁS
+                </button>      
+            </div>
+      </div>
+
+
+
+      <div class="col-lg-4"  style="background-color:white; height: 450px;">       
+            <div class="serv-maritime">
+              <img src="img/maritimo.png" alt="Sercicio de envio maritimo" />
+                <h3>MARÍTIMO</h3>
+                <p>Contamos con cargueros de gran capacidad para que puedas enviar tantos paquetes como gustes con nosotros.</p>
+                <!--Bootstrap Buton-->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#maritimo" style="background-color: #f95908; border:none;">
+                  VER MÁS
+                </button> 
+            </div>
+        
+      </div>
+
+      <div class="col-lg-4"  style="background-color:white; height: 450px;">       
+            <div class="serv-puertaPuerta">
+              <img src="img/puerta.png" alt="servicio de envio puerta a puerta"/>
+                <h3>PUERTA A PUERTA</h3>
+                <p>Llevamos la encomienda que pediste hasta la puerta de hogar u oficina</p>
+                <!--Bootstrap Buton-->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#puerta" style="background-color: #f95908; border:none;">
+                  VER MÁS
+                </button> 
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="row" id="comoEnviar">
+      <div class="col-lg-12"  style="background-color:white; height: 250px; padding-top: 50px;">
+        <div class="componente-envios">
+            <h3>COMO HACER TUS ENVÍOS?</h3>
+            <p style="text-align: center;">¡MUY FACIL!</p>
+        </div>
+      </div>
+    </div>
+
+
+
+     <div class="row">
+    <div class="col-lg-4 paso1Boot">        
+            <div class="paso1">
+                <img src="img/compra.png" width="200" alt="Compra" />
+                <h3 style="font-size: 19px;">REALIZA TU COMPRA</h3>
+                <p>Compra donde quieras</p>
+            </div>
+        
+      </div>
+
+      <div class="col-lg-4"  style="background-color:white; height: 300px;">       
+            <div class="paso2" >
+              <img src="img/oficina.png" width="200" alt="LLevala a la Oficina" />
+                <h3 style="font-size: 19px;">TRAELA A NUESTRAS OFICINAS</h3>
+                 <p>Via España, Final, Vias Porras, Frente a la casa de la batería
+                    <br>
+                    N° Local 20-24
+                 </p>
+            </div>
+        
+      </div>
+
+      <div class="col-lg-4 paso3Boot">       
+            <div class="paso3">
+              <img src="img/paga.png" width="200" alt="Metodos de pago" />
+                <h3 style="font-size: 19px; ">PAGA Y RECIBE TU ENCOMIENDA</h3>
+                <p>Cancela con cualquiera de nuestros metodos de pago</p>
+            </div>
+        </div>
+    </div>
+
+
+
+     <div class="row">
+      <div class="col-lg-12"  style="background-color:white; height: 200px; background-color: none; padding-top: 70px" >
+        <div class="componente-envios">
+            <h3>CONTACTANOS</h3>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+     <div class="row fondo-mozaico">
+      <div class="col-lg-6"  style="background-color:none; height: 550px;">
+        <div class="formulario">
+            <h3>Envianos un Mensaje</h3>
+
+            <form method="post" action="#">
+                <div class="form-group" style="width:300px;">
+                  <label for="exampleFormControlInput1">Tu Nombre</label>
+                  <input class="form-control" id="exampleFormControlInput1" name="nombre">
+                </div>
+
+                <div class="form-group" style="width:300px;">
+                  <label for="exampleFormControlInput1">Email address</label>
+                  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="email">
+                </div>
+
+                <div class="form-group" style="width:300px;">
+                  <label for="exampleFormControlInput1">Teléfono</label>
+                  <input class="form-control" id="exampleFormControlInput1" name="telefono">
+                </div>
+                
+                <div class="form-group ">
+                  <label for="exampleFormControlTextarea1">Tu Mensaje</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="mensaje"></textarea>
+                  <BR>
+                  <input type="submit" name="enviar_solicitud" value="ENVIAR" class="button">
+                </div>
+              </form>
+
+          <?php
+
+ if(isset($_POST['enviar_solicitud'])){
+
+$body='Hemos recibido una solicitud de contacto con los siguientes datos
+      
+  Telefono de Contacto      '.$_POST['nombre'].'                
+  Mensaje:            '.$_POST['telefono'].'
+  Telefono de Contacto      '.$_POST['email'].'               
+  Mensaje:            '.$_POST['mensaje'].'
+  ';
+                
+  $para="encargoenviosyfletes@gmail.com";
+        
+  $mensaje = $body;
+          
+  $asunto   = 'Nueva Solicitud de Servicio';
+  $desde    = $_POST["email"];
+  $mensaje  = $body;
+  $cabeceras = "";
+  $cabeceras = "MIME-VErsion: 1.0 \r\n";
+  $cabeceras  = "Content-Type: text/html; charset=iso-8859-1\r\n";
+  $cabeceras = "To: " . $_POST ["email"] . "\r\n";
+  $cabeceras = "From: " . $_POST ["email"] . "\r\n";    
+              
+              
+  mail ($para, $asunto, $mensaje, $cabeceras);  
+                                      
+  echo "mensaje enviado";
+}
+ ?>
+
+        </div>
+
+
+
+
+
+
+
+      </div>
+      <div class="col-lg-6"  style="background-color:none; height: 550px;">
+        <div class="">
+            <h3>Aquí Estamos</h3>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15763.071634873453!2d-79.5259200626833!3d8.993491643407435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m3!3e4!4m0!4m0!5e0!3m2!1ses-419!2sve!4v1573576241923!5m2!1ses-419!2sve" width="450" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+          
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+    <div class="row" id="contacto">
+    <div class="col-md-3 footerDir">        
+            <div class="redes">
+               
+                <h5 style="font-size: 20px;">DIRECCIÓN</h5>
+                <p>
+                 <img src="img/ubicacion.png" width="20px" alt="ubicacion panama">Ubicación  ciudad de panamá  
+                Via España, Final via Porras
+                frente de la casa de la baterías
+                Numero de local 20-24
+                <br><br>
+                <img src="img/ubicacion.png" width="20px" alt="ubicacion miami">miami 
+                  8140 NW 74 Av, Bay#16 Medley FL, 33166
+                </p>               
+            </div>
+        
+      </div>
+
+      <div class="col-md-3 footerDir" >       
+            <div class="redes">
+                <h5 style="font-size: 20px;">TELÉFONOS</h5>
+                <h6 style="font-size: 20px;">+507 60832916</h6>
+                <h6 style="font-size: 20px;">+507 68310051</h6>
+            </div>
+        
+      </div>
+
+      <div class="col-md-3 footerDir" >       
+            <div class="redes">
+                <h5 style="font-size: 20px;">CORREO ELECTRONICO</h5>
+                <h6>info@encargo.com</h6>
+            </div>
+        </div>
+        <div class="col-md-3 footerDir" >       
+            <div class="redes">
+              <img src="img/gmail.png" width="50px" alt="icono gmail" />         
+              <a href="https://www.facebook.com/encargo-107675347288493/?__tn__=%2Cd%3C-R&eid=ARDfTB93FcL6ZZc6iBdgoPhA1suFm_HDygxVdrtFQRYgfb2Ou_PLfB5PZHBdkGUa4bNARsa4gPCSnaSo" target="_blank"><img src="img/facebook.png" alt ="icono facebook" width="50px"/></a>
+              <a href="https://www.instagram.com/en.cargo" target="_blank"><img src="img/instagram.png" alt="icono instaram" width="50px"/></a>         
+            </div>
+        </div>
+    </div>
+
+
+
+     <div class="row">
+      <div class="col-lg-12"  style="background-color:black; height: 50px;">
+        
+            <h5 style="text-align: center; padding-top: 10px; color:#fa5907; font-size: 17px;">2019 © ENCARGO ENVÍOS Y FLETES</h5>
+      
+      </div>
+    </div>
+
+
+</div><!--Fin Containter Fluid-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
